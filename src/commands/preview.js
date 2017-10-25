80116d12ed8036d04719eeea613a5008855b05b1
Cédric Michaux <he8us@users.noreply.github.com>
2017-10-25 13:05:13 +0200

    ESLint config (#2)
    Code coverage

--- src/commands/preview.js
+++ src/commands/preview.js
@@ -1,8 +1,5 @@
 'use strict';
 
-module.exports = function (file, options) {
-    console.log('file', file);
-    console.log('options', options);
-
-    throw new Error('To implement')
-}
+module.exports = function (){
+    throw new Error('To implement');
+};
