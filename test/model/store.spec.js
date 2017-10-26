19294233dfb9a47b8e2495d77298fa9aedfd2a22
Cedric Michaux <cedric@he8us.be>
2017-10-26 16:58:20 +0200

    Better store tests

--- test/model/store.spec.js
+++ test/model/store.spec.js
@@ -1,80 +1,87 @@
 import {expect} from 'chai';
 import store from '../../src/model/store';
 
-describe('store', () => {
+describe('store reset', () => {
+    it('should empty the store', () => {
+        store._store = {
+            'foo': 'bar'
+        };
+        store.reset();
+        expect(store._store).not.to.have.property('foo');
+    });
+});
 
+describe('store set', () => {
     beforeEach(() => {
         store.reset();
     });
 
-    describe('set', () => {
-        it('should be able to set a key/value', () => {
-            store.set('foo', 'bar');
-            expect(store._store).to.have.property('foo', 'bar');
-        });
-
-        it('should be able to set a multi level key', () => {
-            store.set('foo.bar', 42);
-            expect(store._store).to.have.property('foo');
-            expect(store._store.foo).to.have.property('bar', 42);
-        });
-
-        it('should be able to set a multi level key', () => {
-            store.set('foo.bar.deep', 42);
-            expect(store._store).to.have.property('foo');
-            expect(store._store.foo).to.have.property('bar');
-            expect(store._store.foo.bar).to.have.property('deep', 42);
-        });
-
-        it('should be able to set multiple multi level key', () => {
-            store.set('foo.bar.deep', 42);
-            store.set('foo.baz.deep', 43);
-            expect(store._store).to.have.property('foo');
-            expect(store._store.foo).to.have.property('bar');
-            expect(store._store.foo).to.have.property('baz');
-            expect(store._store.foo.bar).to.have.property('deep', 42);
-            expect(store._store.foo.baz).to.have.property('deep', 43);
-        });
-
+    it('should be able to set a key/value', () => {
+        store.set('foo', 'bar');
+        expect(store._store).to.have.property('foo', 'bar');
     });
 
-    describe('get', () => {
-        it('should be able to recover a value from the store', () => {
-            store._store = {'foo': 'bar'};
-            expect(store.get('foo')).to.equal('bar');
-        });
-
-        it('should be able to read a multi level key from the store', () => {
-            store._store = {'foo': {'bar': {'deep': 42}}};
-
-            let value = store.get('foo.bar');
-            expect(value).to.have.property('deep', 42);
-        });
-
-        it('should be able to read a multi level key from the store', () => {
-            store._store = {'foo': {'bar': {'deep': 42}}};
-            expect(store.get('foo.bar.deep')).to.equal(42);
-        });
-
-        it('should return the whole store if no key is given', () => {
-            store._store = {'foo': 'bar', 'foo2': 42};
-            let value = store.get();
-            expect(value).to.have.property('foo', 'bar');
-            expect(value).to.have.property('foo2', 42);
-        });
-
-        it('should return the default value if the key is undefined', () => {
-            store._store = {};
-            expect(store.get('foo.bar', 42)).to.equal(42);
-        });
+    it('should be able to set a multi level key', () => {
+        store.set('foo.bar', 42);
+        expect(store._store).to.have.property('foo');
+        expect(store._store.foo).to.have.property('bar', 42);
     });
 
-    describe('reset', () => {
-        it('should empty the store', () => {
-            store._store = {'foo': 'bar'};
-            store.reset();
-            expect(store._store).not.to.have.property('foo');
-        });
+    it('should be able to set a multi level key', () => {
+        store.set('foo.bar.deep', 42);
+        expect(store._store).to.have.property('foo');
+        expect(store._store.foo).to.have.property('bar');
+        expect(store._store.foo.bar).to.have.property('deep', 42);
     });
 
+    it('should be able to set multiple multi level key', () => {
+        store.set('foo.bar.deep', 42);
+        store.set('foo.baz.deep', 43);
+        expect(store._store).to.have.property('foo');
+        expect(store._store.foo).to.have.property('bar');
+        expect(store._store.foo).to.have.property('baz');
+        expect(store._store.foo.bar).to.have.property('deep', 42);
+        expect(store._store.foo.baz).to.have.property('deep', 43);
+    });
+});
+
+
+describe('store get', () => {
+
+    beforeEach(() => {
+        store.reset();
+        store._store = {
+            'foo': {
+                'bar': {
+                    'baz': 42
+                }
+            },
+            'foobar': 'raboof',
+            'foobaz': 'zaboof'
+        };
+    });
+
+
+    it('should be able to recover a value from the store', () => {
+        expect(store.get('foobar')).to.equal('raboof');
+    });
+
+    it('should be able to read a multi level key from the store', () => {
+        let value = store.get('foo.bar');
+        expect(value).to.have.property('baz', 42);
+    });
+
+    it('should be able to read a multi level key from the store', () => {
+        expect(store.get('foo.bar.baz')).to.equal(42);
+    });
+
+    it('should return the whole store if no key is given', () => {
+        let value = store.get();
+        expect(value).to.have.property('foobar', 'raboof');
+        expect(value).to.have.property('foobaz', 'zaboof');
+    });
+
+    it('should return the default value if the key is undefined', () => {
+        expect(store.get('unknown', 42)).to.equal(42);
+    });
 });
